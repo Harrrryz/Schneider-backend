a84a0ad79fed8c9cc7e5b6d74e13d5efa39d2690
Harrrryz <harry.wadu233@gmail.com>
2025-02-28 04:34:45 -0500

    update access token payload to include user ID and refresh SchneiderDatabase.db

--- src/backend/main.py
+++ src/backend/main.py
@@ -317,7 +317,7 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> T
 
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.userName}, expires_delta=access_token_expires
+        data={"sub": user.userName, "id": user.id}, expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type="bearer")
 
